163175b75918af6c210008352fcf19c43e080bd3
Gursimranjit Singh <gursimranjit1196@gmail.com>
2020-05-31 16:04:21 +0530

    Updates

---
+++ precache-manifest.ca86197cc2ac934ddcf37df610b8cf05.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032f92c9fd1b58d2722368ddf5c548b7",
+    "url": "/gursimranjit1196/index.html"
+  },
+  {
+    "revision": "8f90f166e81101e3db83",
+    "url": "/gursimranjit1196/static/css/main.f7419275.chunk.css"
+  },
+  {
+    "revision": "38a7484e1e359241ba15",
+    "url": "/gursimranjit1196/static/js/2.3756b644.chunk.js"
+  },
+  {
+    "revision": "8f90f166e81101e3db83",
+    "url": "/gursimranjit1196/static/js/main.03e923da.chunk.js"
+  },
+  {
+    "revision": "c85002e6d9635b9ad3c7",
+    "url": "/gursimranjit1196/static/js/runtime-main.d1bb313a.js"
+  },
+  {
+    "revision": "c041404a08f53ac5636197bb94a81396",
+    "url": "/gursimranjit1196/static/media/AWS-SolArchitect-Associate.c041404a.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/gursimranjit1196/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/gursimranjit1196/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/gursimranjit1196/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/gursimranjit1196/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "6dcc9bda337575f9ad146f7c175fdd0f",
+    "url": "/gursimranjit1196/static/media/me.6dcc9bda.jpeg"
+  }
+]);
